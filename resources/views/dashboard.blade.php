c7a0dec28c4a751aa301467d82e2c9f0699564cb
forentika <forentkaa@gmail.com>
2025-04-24 00:10:49 +0700

    lanjutan breeze

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,6 +10,11 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
+                    <div class="mt-4">
+                        <a href="{{ route('posts.index') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                            Go to Posts
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
